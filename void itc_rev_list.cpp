eb7d15cc3917492c8546dda5f9441bff4ab807fc
Ewrhey <92299233+Ewrhey@users.noreply.github.com>
2022-01-20 12:16:08 +0300

    Update void itc_rev_list.cpp

--- void itc_rev_list.cpp
+++ void itc_rev_list.cpp
@@ -1,9 +1,8 @@
 #include "easy_list.h"
 void itc_rev_list(vector <int> &mass){
     if (mass.size() > 0){
-    len = mass.size()/2;
     int sup = 0;
-    for (int i = 0 ; i <= (len); i++){
+    for (int i = 0 ; i <= (mass.size()/2); i++){
         sup = mass[i];
         mass[i] = mass[(mass.size() - 1) - i];
         mass[(mass.size() - 1) - i] = sup;
